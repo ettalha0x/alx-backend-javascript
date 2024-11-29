cbdf2533a6e70624ee322fe54c78f25248f2c753
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-11-29 17:17:56 +0100

    1-stin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-process.stdout.write('Welcome to Holberton School, what is your name? \n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
     const name = process.stdin.read();
